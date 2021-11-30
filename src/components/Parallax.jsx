ede5b37e43594448f338a27ebc37a258d850a500
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2021-11-30 13:21:06 +0100

    start

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -6,7 +6,7 @@ export default function Parallax() {
     <div className="Parallax">
       {/* <img src="img/background-plan.jpg" alt="background hero" id="bg-prallax"/>
             <img src="img/first-plan.jpg" alt="front parallax" id="front-parallax"></img> */}
-      <img className="bgHome" src="img/bg.jpg" alt="background mountain"></img>
+      {/* <img className="bgHome" src="img/bg.jpg" alt="background mountain"></img> */}
       <h2>Kévin Lemarié</h2>
       <h1>Développeur Web Fullstack</h1>
     </div>
